4f8520ea4256cd5dfa2f80871bd1c747384deb1d
YudaiTsukamoto <yudai.sr400@gmail.com>
2021-04-15 20:47:39 +0900

    Create User

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import Adapters from 'next-auth/adapters'
+import { prisma } from '../../../lib/prisma'
 
 export default NextAuth({
   providers: [
@@ -8,4 +10,19 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      console.log(user, account, profile)
+      return true
+    },
+  },
+  adapter: Adapters.Prisma.Adapter({
+    prisma,
+    modelMapping: {
+      User: 'user',
+      Account: 'account',
+      Session: 'session',
+      VerificationRequest: 'verificationRequest'
+    }
+  }),
 })

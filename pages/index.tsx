3c43af3ebcfaae538e7e26d3c4181bc3352e617d
YudaiTsukamoto <yudai.sr400@gmail.com>
2021-04-15 19:27:10 +0900

    ログアウト出来るようにした

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,20 @@
 import Head from 'next/head'
 import { Top } from '../components/templates/Top'
-import { useSession } from 'next-auth/client'
-import { Spinner } from '@chakra-ui/react'
+import { useSession, signOut } from 'next-auth/client'
+import { Spinner, Box, Button } from '@chakra-ui/react'
 
 const Home: React.FC = () => {
   const [session, loading] = useSession()
+  console.log(session);
 
   if (session) {
-    return <div>ログインしてますねあなた</div>
+    return (
+      <Box>
+        <Box mb={10}>ログインしてますねあなた</Box>
+        <Box mb={10}>{String(session)}</Box>
+        <Button onClick={() => signOut()}>ログアウト</Button>
+      </Box>
+    )
   }
 
   if (loading) {

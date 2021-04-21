862cbd6b5b2ac99007f2295dccdd668e62f4c191
YudaiTsukamoto <yudai.sr400@gmail.com>
2021-04-21 10:47:00 +0900

    ゴミコードを消した

--- @types/TypeUtil.d.ts
+++ @types/TypeUtil.d.ts
@@ -2,11 +2,7 @@ declare namespace TypeUtil {
   type Dispatchers<T extends ReturnDispatchers> = valueOf<
     Pick<ReturnType<T>, 'dispatchers'>
   >
-  type Selectors<T extends ReturnSelectors> = valueOf<
-    Pick<ReturnType<T>, 'selectors'>
-  >
   type ReturnDispatchers = (args: unknown) => { dispatchers: unknown }
-  type ReturnSelectors = (args: unknown) => { selectors: unknown }
   type valueOf<T> = T[keyof T]
   type RequireOne<T, K extends keyof T = keyof T> = K extends keyof T
     ? PartialRequire<T, K>

1d68f5cd2d6ae76dfdf9fb618c87ecb314e642fe
YudaiTsukamoto <yudai.sr400@gmail.com>
2021-04-22 14:20:35 +0900

    Listモデルを作った

---
+++ prisma/migrations/20210422051754_add_list_model/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `lists` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `board_id` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `lists` ADD FOREIGN KEY (`board_id`) REFERENCES `boards`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;

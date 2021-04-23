e65cde93af7d9e189ce5e59a4c64852db0af4041
YudaiTsukamoto <yudai.sr400@gmail.com>
2021-04-23 09:56:56 +0900

    add usecase of update list

--- lib/server/useCase/list.ts
+++ lib/server/useCase/list.ts
@@ -1,6 +1,6 @@
 import { plainToClass } from 'class-transformer'
 import { List } from '@prisma/client'
-import { CreateList, ResponseList } from '../../../dto/list'
+import { CreateList, ResponseList, UpdateList } from '../../../dto/list'
 import { createListRequest, getListsRequest } from '../repositories/list'
 
 export const createList = async (
@@ -13,6 +13,11 @@ export const createList = async (
 export const getLists = async (boardId: number): Promise<ResponseList[]> => {
   const data = await getListsRequest(boardId)
   return transformClass(data) as ResponseList[]
+}
+
+export const updateList = async (dto: UpdateList): Promise<ResponseList> => {
+  const data = await updateListRequest(dto)
+  return transformClass(data) as ResponseList
 }
 
 const transformClass = (data: List | List[]): ResponseList | ResponseList[] => {

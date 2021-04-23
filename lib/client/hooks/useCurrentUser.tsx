8ff614078aa5adc84653af69543d322244dee93c
YudaiTsukamoto <yudai.sr400@gmail.com>
2021-04-23 09:47:47 +0900

    fix bug

--- lib/client/hooks/useCurrentUser.tsx
+++ lib/client/hooks/useCurrentUser.tsx
@@ -1,7 +1,7 @@
 import { useContext, createContext } from 'react'
 import { CurrentUser } from '../../../model/client/CurrentUser'
 
-export const CurrentUserContext = createContext<CurrentUser>(undefined)
+export const CurrentUserContext = createContext<CurrentUser>({} as CurrentUser)
 
 export const useCurrentUser = (): CurrentUser => useContext(CurrentUserContext)
 

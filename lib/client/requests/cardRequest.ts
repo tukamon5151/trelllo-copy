344ec48a28de0940c5bc99e3d1f103187067f664
YudaiTsukamoto <yudai.sr400@gmail.com>
2021-05-08 16:36:29 +0900

    add card request

---
+++ lib/client/requests/cardRequest.ts
@@ -0,0 +1,22 @@
+import { plainToClass } from 'class-transformer'
+import { Card } from '../../../model/client/Card'
+import { CreateCard, ResponseCard } from '../../../dto/card'
+import { postRequest } from './request'
+
+type CreateCardResponse = {
+  card: ResponseCard
+}
+
+export const createCardRequest = async (
+  listId: number,
+  card: CreateCard,
+): Promise<Card> => {
+  const response = (await postRequest(
+    `/api/lists/${listId}/cards`,
+    JSON.stringify({ card }),
+  )) as CreateCardResponse
+  return transformClass(response.card) as Card
+}
+
+const transformClass = (data: ResponseCard | ResponseCard[]): Card | Card[] =>
+  plainToClass(Card, data, { excludeExtraneousValues: true })
